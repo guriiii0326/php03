6b4f58c5c23d1b2f2c3a668f17b36b606db8ab00
T-ooguri <bmff0326@outlook.jp>
2021-11-22 20:26:30 +0000

    laravel03

--- 03.php
+++ 03.php
@@ -1,6 +1,6 @@
 <?php 
 $name="Tomoka";
-if($name="Tomoka"){
+if($name=="Tomoka"){
     echo "私は".$name."です";
 }else{
     echo $name."ではありません";
@@ -24,3 +24,12 @@ for($i=$start;$i<=$end;$i++){
         echo "\n".$i."\n";
     }
 }
+
+$i=array(2,3,5,4,6);
+foreach($i as $o){
+    $a=0;
+    if($a<$o){
+        $a=$o;
+    }
+}
+echo "\n".$a;
